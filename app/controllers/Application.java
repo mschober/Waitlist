f0d9d7fe1f12ab3d5695ea9997134158dee1415b
Michael Schober <mynetlogins.schober@gmail.com>
2012-03-23 20:37:10 -0700

    fname lname make it to db, page refreshes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -10,14 +11,23 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-    	Applicant kim = new Applicant("Kim", "Pohl");
-    	kim.save();
-    	List<Applicant> applicants = Applicant.find("date >= '2012-03-21 22:00:00, yyyy-mm-dd HH:mm:ss' AND date <= '2012-03-24 22:00:30, yyyy-mm-dd HH:mm:ss' and id > ? order by id desc", 1l).fetch();
-        render(applicants);
+//    	Applicant kim = new Applicant("Kim", "Pohl");
+//    	kim.save();
+//    	List<Applicant> applicants = Applicant.find("date >= '2012-03-21 22:00:00, yyyy-mm-dd HH:mm:ss' AND date <= '2012-03-24 22:00:30, yyyy-mm-dd HH:mm:ss' and id > ? order by id desc", 1l).fetch();
+//        render(applicants);
+    	render();
     }
     
     public static void que(){
     	render();
+    }
+    
+    public static void post(@Required String fname, @Required String lname){
+    	if(validation.hasErrors())
+    		System.out.println("has errors");
+    	
+    	new Applicant(fname, lname).save();
+    	index();
     }
 
 }
