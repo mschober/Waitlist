a838ea575fb85724639618ce0ec67a812a0a5ddb
Michael Schober <mynetlogins.schober@gmail.com>
2012-03-23 21:18:04 -0700

    fixed error where it would not connect to database:

---
+++ test/utils/TestApplicantHelper.java
@@ -0,0 +1,18 @@
+package utils;
+
+import models.Applicant;
+
+public class TestApplicantHelper {
+
+	public static final String BY_LAST_NAME = "byLastName";
+	public static final String BY_FIRST_NAME = "byFirstName";
+	public static final String SCHOBER = "Schober";
+	public static final String MICHAEL = "Michael";
+
+	public static Applicant findByFirstName(String michael) {
+		return Applicant.find(BY_FIRST_NAME, MICHAEL).first();
+	}
+	
+	
+
+}
