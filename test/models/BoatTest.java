ece0110d97a80e96e2aef2d5036b5d8200bd5571
Michael Schober <mynetlogins.schober@gmail.com>
2012-03-28 18:10:45 -0700

    adding boiler plate code for boat test

--- test/models/BoatTest.java
+++ test/models/BoatTest.java
@@ -2,18 +2,27 @@ package models;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import play.db.jpa.GenericModel.JPAQuery;
+import play.test.Fixtures;
 import play.test.UnitTest;
 
 public class BoatTest extends UnitTest {
 
 	private Boat powerBoat;
+	private Boat sailBoat;
 
+	@Before
+	public void setup(){
+		Fixtures.deleteDatabase();
+		powerBoat = new Boat("power").save();
+		sailBoat = new Boat("sail").save();
+	}
+	
 	@Test
 	public void exists() {
-		powerBoat = new Boat("power");
 		assertNotNull(powerBoat);
 	}
 	
@@ -21,6 +30,12 @@ public class BoatTest extends UnitTest {
 	public void isPowerBoat(){
 		Boat found = Boat.find("byType", "power").first();
 		assertEquals(powerBoat, found);
+	}
+	
+	@Test
+	public void isSailBoat(){
+		Boat found = Boat.find("byType", "sail").first();
+		assertEquals(sailBoat, found);
 	}
 
 }
